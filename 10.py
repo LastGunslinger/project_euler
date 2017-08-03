33551e931ec70484f50e66d37b4689ae399bb7ae
Reginald Johnson <Reginald.Johnson@cpa.texas.gov>
2017-08-03 16:35:31 -0500

    Ugh

--- 10.py
+++ 10.py
@@ -8,49 +8,66 @@ Find the sum of all the primes below two million.
 import time
 
 def main():
+	limit = 2000000
+	return sieve_of_Reggie(limit)
 
-	return sieve_of_Eratosthenes(2000000)
+	# return sieve_of_Eratosthenes(limit)
 
-	primes = [2]
-	current_num = 1
-	while primes[-1] < 2000000:
-		print(primes[-1])
-		current_num += 2
-		x = 0
-		isPrime = True
-		#while x < len(primes):
-		while primes[x] < current_num/2:
-			if current_num % primes[x] == 0:
-				isPrime = False
-				break
-			x += 1
-		if isPrime:
-			primes.append(current_num)
-	#print(primes)
-	return sum([x for x in primes if x < 2000000])
+def sieve_of_Reggie(limit):
+	primes = [x for x in range(2,limit)]
+	primes = [primes[0]] + [primes[x] for x in range(1,len(primes), 2)]
+
+	x = 1
+	while x < len(primes):
+		if isPrime(primes[x]):
+			prime_number = primes[x]
+			print(prime_number)
+			index = x + 1
+			while index < len(primes):
+				#print(primes[index])
+				if primes[index] % prime_number == 0:
+					primes.remove(primes[index])
+				else:
+					index += 1
+		x += 1
+	return sum(primes)
+
+def isPrime(number):
+	x = 2
+	while x < number/2:
+		if number % x == 0:
+			return False
+		x += 1
+	return True
 
 def sieve_of_Eratosthenes(limit):
 	primes = [x for x in range(2, limit)]
 	prime_flags = [None for x in primes]
-	
-	index = 0
+	prime_flags[0] = True
+	for x in range(2, len(prime_flags), 2):
+		prime_flags[x] = False
+	for x in range(3, len(prime_flags), 5):
+		prime_flags[x] = False
+
+	index = 1
 	while None in prime_flags and index < len(primes):
-		print(primes[index])
-		if prime_flags[index] is not None:
-			if is_prime(primes[index]):
+		if prime_flags[index] is None:
+			if is_prime(index, primes, prime_flags):
+				print(primes[index])
 				prime_flags[index] = True
-				for flag in range(index, len(primes), index + 1):
-					prime_flags[flag] = False
+				for flag in range(index + primes[index], len(primes), primes[index]):
+					if prime_flags[flag] == None:
+						prime_flags[flag] = False
 			else:
 				prime_flags[index] = False
-		index += 1
+		index += 2
+	return sum([x[0] for x in zip(primes, prime_flags) if x[1]])
 
-	print([x for x in primes if prime_flags[x]])
+def is_prime(index, primes, prime_flags):
+	x = 0
 
-def is_prime(num):
-	x = 2
-	while x < num / 2:
-		if x % num == 0:
+	while primes[x] < primes[index]/2:
+		if prime_flags[x] and primes[index] % primes[x] == 0:
 			return False
 		x += 1
 	return True
