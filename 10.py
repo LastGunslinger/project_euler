87622c10ebde8057c746cbb12dc8e3f49cb382ca
Reginald Johnson <rjoh716@l43687.core.cpa.state.tx.us>
2017-08-03 23:27:16 -0500

    Finished 10 and 11

--- 10.py
+++ 10.py
@@ -15,64 +15,30 @@ def main():
 	# return sieve_of_Eratosthenes(limit)
 
 def sieve_of_Reggie(limit):
-	primes = [x for x in range(2,limit)]
-	primes = [primes[0]] + [primes[x] for x in range(1,len(primes), 2)]
-
+	primes = [2, 3] + [x for x in range(5,limit, 2)]
+	# print(primes)
 	x = 1
 	while x < len(primes):
 		if isPrime(primes[x], primes):
 			prime_number = primes[x]
-			print(prime_number)
-			index = x + 1
-			while index < len(primes):
-				#print(primes[index])
-				if primes[index] % prime_number == 0:
-					primes.remove(primes[index])
-				else:
-					index += 1
+			print(f'{prime_number} - {len(primes)} potential primes left')
+			new_primes = primes[:x+1] + [ num for num in primes[x+1:] if num % prime_number != 0 ]
+			if len(new_primes) == len(primes):
+				break
+			else:
+				primes = new_primes
+			# print(primes)
 		x += 1
+	print(primes)
 	return sum(primes)
 
 def isPrime(number, primes):
-	x = 0
-	while primes[x] < math.sqrt(number):
+	x = 1
+	while primes[x] <= math.sqrt(number):
 		if number % primes[x] == 0:
 			return False
 		x += 1
 	return True
-
-def sieve_of_Eratosthenes(limit):
-	primes = [x for x in range(2, limit)]
-	prime_flags = [None for x in primes]
-	prime_flags[0] = True
-	for x in range(2, len(prime_flags), 2):
-		prime_flags[x] = False
-	for x in range(3, len(prime_flags), 5):
-		prime_flags[x] = False
-
-	index = 1
-	while None in prime_flags and index < len(primes):
-		if prime_flags[index] is None:
-			if is_prime(index, primes, prime_flags):
-				print(primes[index])
-				prime_flags[index] = True
-				for flag in range(index + primes[index], len(primes), primes[index]):
-					if prime_flags[flag] == None:
-						prime_flags[flag] = False
-			else:
-				prime_flags[index] = False
-		index += 2
-	return sum([x[0] for x in zip(primes, prime_flags) if x[1]])
-
-def is_prime(index, primes, prime_flags):
-	x = 0
-
-	while primes[x] < primes[index]/2:
-		if prime_flags[x] and primes[index] % primes[x] == 0:
-			return False
-		x += 1
-	return True
-
 
 if __name__ == '__main__':
 	start = time.time()
