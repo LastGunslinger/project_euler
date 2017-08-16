a230e2e0c0b7d671633bd591382808740f70a55e
Reginald Johnson <Reginald.Johnson@cpa.texas.gov>
2017-08-16 16:31:42 -0500

    Completed 27

--- 26.py
+++ 26.py
@@ -17,6 +17,41 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 import time
 import pytest
 import re
+import math
+
+def is_prime(number):
+	if 1 < number <= 3:
+		return True
+	for x in range(2, math.ceil(math.sqrt(number) + 1 )):
+		if number % x == 0:
+			return False
+	return True
+
+def largest_prime(limit):
+	prime = 2
+	for divisor in range(2, limit):
+		if is_prime(divisor):
+			prime = divisor
+	return prime
+
+def find_repeat_length(divisor):
+	'''
+	Let n < d, and you're trying to figure out the repeating part of n/d. Let p be the number of digits in the repeating part: then n/d = R * 10^(-p) + R * 10^(-2p) + ... = R * ((10^-p)^1 + (10^-p)^2 + ...). The bracketed part is a geometric series, equal to 1/(10^p - 1).
+	So n / d = R / (10^p - 1). Rearrange to get R = n * (10^p - 1) / d. To find R, loop p from 1 to infinity, and stop as soon as d evenly divides n * (10^p - 1).
+	'''
+	dividend = 1 * 9
+	new_dividend = dividend
+	divisor = 2
+	repeating_length = 1
+	while new_dividend % divisor:
+		new_dividend = new_dividend * 10 + dividend
+		repeating_length += 1
+	return repeating_length
+
+def test_find_repeat_length():
+	assert find_repeat_length(7) == 6
+	assert find_repeat_length(263) == 262
+
 
 def long_divide(dividend, divisor):
 	pattern = re.compile(r'(\d+)\1{3,}', re.IGNORECASE)
@@ -50,7 +85,15 @@ def test_main():
 	assert long_divide(1, 4) == ('0.25', 0)
 	assert long_divide(1, 5) == ('0.2', 0)
 	assert long_divide(1, 7) == ('0.(142857)', 6)
-	main(15)
+	main(50)
+
+def test_is_prime():
+	assert is_prime(43) == True
+	assert is_prime(18) == False
+
+def test_largest_prime():
+	assert largest_prime(20) == 19
+	print(largest_prime(1000))
 
 if __name__ == '__main__':
 	start = time.time()
---
+++ 27.py
@@ -0,0 +1,53 @@
+import time
+import pytest
+import math
+
+def quadratic(x, a, b):
+	return sum([x**2, a*x, b])
+
+def test_Quadratic():
+	assert quadratic(3, -79, 1601) == 1373
+
+def is_prime(number):
+	if number < 2:
+		return False
+	elif number == 2:
+		return True
+	else:
+		for x in range(2, math.ceil(math.sqrt(number) + 1 )):
+			if number % x == 0:
+				return False
+		return True
+
+def test_is_prime():
+	assert is_prime(2) == True
+	assert is_prime(3) == True
+	assert is_prime(18) == False
+	assert is_prime(27) == False
+
+def main(a_limit, b_limit):
+	prime_count = 0
+	coeffs = (None, None)
+	for a in range(-a_limit + 1, a_limit):
+		for b in range(-b_limit, b_limit + 1):
+			x = 0
+			q = quadratic(x, a, b)
+			primes = []
+			while( is_prime(q) ):
+				primes.append(q)
+				x += 1
+				q = quadratic(x, a, b)
+			if len(primes) > prime_count:
+				prime_count = len(primes)
+				coeffs = (a, b)
+				print(f'x^2 + ({a})x + ({b}) = {primes}')
+	return coeffs, prime_count
+
+def test_main():
+	coeffs, prime_count = main(10, 10)
+	print(coeffs, prime_count)
+
+if __name__ == '__main__':
+	start = time.time()
+	print(main(1000, 1000))
+	print('--- {} seconds ---'.format(time.time()-start))
