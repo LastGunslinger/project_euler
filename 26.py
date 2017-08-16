d09c21123d95f9d85337a73101ef41f49081fe9d
Reginald Johnson <Reginald.Johnson@cpa.texas.gov>
2017-08-15 20:26:01 -0500

    Updated 26

--- 26.py
+++ 26.py
@@ -16,12 +16,41 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 '''
 import time
 import pytest
+import re
 
-def main():
-	return
+def long_divide(dividend, divisor):
+	pattern = re.compile(r'(\d+)\1{3,}', re.IGNORECASE)
+	quotient = ''
+	remainder = 1
+	while remainder != 0 :
+		quotient += str(int(dividend / divisor))
+		if quotient == '0':
+			quotient += '.'
+		remainder = dividend % divisor
+		dividend = remainder * 10
+		match = pattern.search(quotient)
+		if match and quotient[-1] != '0':
+			quotient = re.sub(r'{}+'.format(match.group()), r'({})'.format(match.group(1)), quotient)
+			# print(match.group(1))
+			return quotient, len(match.group(1))
+	return quotient, 0
+
+def main(limit):
+	longest_recurring = 7
+	recurring_length = 6
+	for x in range(1, limit):
+		q, length = long_divide(1, x)
+		if length > recurring_length:
+			recurring_length = length
+			longest_recurring = x
+			print(f'1 / {x} : {recurring_length}')
+	return longest_recurring
 
 def test_main():
-	pass
+	assert long_divide(1, 4) == ('0.25', 0)
+	assert long_divide(1, 5) == ('0.2', 0)
+	assert long_divide(1, 7) == ('0.(142857)', 6)
+	main(15)
 
 if __name__ == '__main__':
 	start = time.time()
