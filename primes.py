686086f26e6377ab31d551d31b67f9c9597ff26a
Reginald Johnson <rjoh716@l71117.core.cpa.state.tx.us>
2018-06-26 23:25:02 -0500

    Removed old pipfiles.

--- primes.py
+++ primes.py
@@ -3,53 +3,53 @@ import math
 import pytest
 
 class PrimeIterator:
-	def __init__(self, sentinel=None):
-		self.prime_list = []
-		self._count = 1
-		self.sentinel = sentinel
+    def __init__(self, sentinel=None):
+        self.prime_list = []
+        self._count = 1
+        self.sentinel = sentinel
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		while True:
-			self._count += 1
-			if self.sentinel and self._count >= self.sentinel:
-				raise StopIteration
-			for prime in [x for x in self.prime_list if x < math.sqrt(self._count) + 1]:
-				if self._count % prime == 0:
-					break
-			else:
-				self.prime_list.append(self._count)
-				print(self._count)
-				return self._count
+    def __next__(self):
+        while True:
+            self._count += 1
+            if self.sentinel and self._count >= self.sentinel:
+                raise StopIteration
+            for prime in [x for x in self.prime_list if x < math.sqrt(self._count) + 1]:
+                if self._count % prime == 0:
+                    break
+            else:
+                self.prime_list.append(self._count)
+                print(self._count)
+                return self._count
 
 def gen_primes():
-	yield 2
-	yield 3
-	primes = [2, 3]
-	for num in itertools.count(start=5, step=2):
-		for prime in filter(lambda x: x < math.sqrt(num) + 1, primes):
-			if num % prime == 0:
-				break
-		else:
-			primes.append(num)
-			yield num
+    yield 2
+    yield 3
+    primes = [2, 3]
+    for num in itertools.count(start=5, step=2):
+        for prime in filter(lambda x: x < math.sqrt(num) + 1, primes):
+            if num % prime == 0:
+                break
+        else:
+            primes.append(num)
+            yield num
 
 def test_gen_primes():
-	for x in gen_primes():
-		print(x)
-		if x > 50:
-			break
+    for x in gen_primes():
+        print(x)
+        if x > 50:
+            break
 
 
 def is_prime(number):
-	if number < 2:
-		return False
-	for x in range(2, int(math.sqrt(number)) + 1):
-		if number % x == 0:
-			return False
-	return True
+    if number < 2:
+        return False
+    for x in range(2, int(math.sqrt(number)) + 1):
+        if number % x == 0:
+            return False
+    return True
 
 @pytest.mark.parametrize("test_input, expected", [
     (2, True),
@@ -61,6 +61,6 @@ def is_prime(number):
 ])
 
 def test_is_prime(test_input, expected):
-	is_prime(test_input)
-			
-		
+    is_prime(test_input)
+            
+        
