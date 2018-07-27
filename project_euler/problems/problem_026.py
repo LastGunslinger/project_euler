f420b66c93b0d7b6e2dcbc18d21e7fad2539eb2c
Reggie Johnson <reginald.johnson+github@outlook.com>
2018-07-26 23:01:22 -0500

    Broke 26 even more!

--- project_euler/problems/problem_026.py
+++ project_euler/problems/problem_026.py
@@ -18,20 +18,23 @@ import time
 import pytest
 import re
 import math
-from ..utilities import is_prime
+from ..utilities import is_prime, int_list
+from typing import List
 
 
 def solve():
+    assert divide(1, 11) == 2
+    assert divide(1, 12) == 1
     limit = 1000
-    longest_recurring = 7
-    recurring_length = 6
-    for x in range(1, limit):
-        q, length = long_divide(1, x)
-        if length > recurring_length:
-            recurring_length = length
-            longest_recurring = x
-            print(f'1 / {x} : {recurring_length}')
-    return longest_recurring
+    value = 7
+    longest_recurring = 6
+    for x in range(11, limit):
+        recurring_length = divide(1, x)
+        print(f'1 / {x} : {recurring_length}')
+        if recurring_length > longest_recurring:
+            longest_recurring = recurring_length
+            value = x
+    return value
 
 
 def long_divide(dividend, divisor):
@@ -50,3 +53,47 @@ def long_divide(dividend, divisor):
             # print(match.group(1))
             return quotient, len(match.group(1))
     return quotient, 0
+
+
+def divide(dividend: int, divisor: int):
+    dividend = int_list(dividend)
+    quotient = []
+
+    while True:
+        while divisor > dividend[0]:
+            if len(dividend) > 1:
+                dividend[0] = int(str(dividend[0]) + str(dividend.pop(1)))
+            else:
+                dividend[0] *= 10
+                quotient.append(0)
+
+        quotient.append(int(dividend[0] / divisor))
+        remainder = dividend[0] % divisor
+        if remainder:
+            dividend[0] -= (divisor * int(dividend[0] / divisor))
+            seq = seq_len(quotient)
+            if seq:
+                return seq
+        else:
+            return 0
+
+
+def guess_seq_len(seq: List[int]) -> int:
+    guess = None
+    max_len = int(len(seq) / 2)
+    for x in range(2, max_len):
+        if seq[0:x] == seq[x:2 * x]:
+            guess = x
+
+    return guess
+
+
+def seq_len(seq: int) -> int:
+    # print(seq)
+    search_str = ''.join(str(x) for x in seq)
+    match = re.search(r'0+(?P<repeat>\d+)(\1)', search_str)
+    if match:
+        print(match.group('repeat'))
+        return len(match.group('repeat'))
+    else:
+        return 0
