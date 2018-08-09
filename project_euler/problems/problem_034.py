900c091e720862b785ddb3696746478b7792bd65
Reginald Johnson <rjoh716@l71276.core.cpa.state.tx.us>
2018-08-09 18:05:03 -0500

    Added more logging for problems <= 36

--- project_euler/problems/problem_034.py
+++ project_euler/problems/problem_034.py
@@ -1,4 +1,5 @@
 prompt = '''
+
 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 
 Find the sum of all numbers which are equal to the sum of the factorial of their digits.
@@ -6,9 +7,7 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
 '''
-import time
 from math import factorial
-from termcolor import colored
 
 
 def factorial_sum(number: int) -> int:
@@ -34,12 +33,6 @@ def solve(logger):
         if factorial(largest_digit(x)) > x:
             continue
         if x == factorial_sum(x):
-            print(f'{"! + ".join(str(x))}! = {x}')
+            logger.debug(f'{"! + ".join(str(x))}! = {x}')
             factorial_sums.append(x)
     return sum(factorial_sums)
-
-
-if __name__ == '__main__':
-    start = time.time()
-    print(f'Result: {colored(main(), "green")}')
-    print('--- {} seconds ---'.format(time.time() - start))
